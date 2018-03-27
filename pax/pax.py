36259a067a080d8d11d947e5caed6cfa44007bc0
Zlkva <keithfgilbert@gmail.com>
2018-03-27 13:02:58 -0400

    Create pax.py

---
+++ pax/pax.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+import requests
+from bs4 import BeautifulSoup
+import re
+import signal
+import sys
+import time
+
+class paxcheck:
+    """Checks paxsite.como for West date changes"""
+
+    run = True
+
+    def __init__(self, bot):
+            self.bot = bot
+            self.green = discord.Color.green()
+            self.orange = discord.Color.orange()
+            self.red = discord.Color.red()
+            self.blue = discord.Color.blue()
+
+    @commands.command()
+        async def pax(self):
+
+        while run:
+            website = 'http://www.paxsite.com/'
+            r = requests.get(website)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            soup2 = BeautifulSoup(r.content, 'html.parser')
+            for datapull in soup.findAll("h3"):
+
+            if "2017" in datapull.text:
+                pulledtext = datapull.text
+                embed = discord.Embed(title="**No Changes Yet**", description=pulledtext, color=self.red)
+                await self.bot.say(embed=embed)
+                time.sleep(1800)
+
+            elif "2018" in datapull.text:
+                pulledtext = datapull.text
+                sayme = "@everyone"
+                sayme =+ pulledtext
+                embed = discord.Embed(title=":tada: **IT'S TIME** :tada:", description=sayme, color=self.green)
+                await self.bot.say(embed=embed)
+                time.sleep(10)
+            else:
+                break
